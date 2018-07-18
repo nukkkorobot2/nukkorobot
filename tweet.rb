4cb7bb1792eb530b7d93a8b30c8e8ac06e47f6d1
nukkorobot2 <ymneko3104snow@gmail.com>
2018-07-18 23:29:33 +0900

    9th commit

--- tweet.rb
+++ tweet.rb
@@ -25,12 +25,10 @@ stream_client.user do |tweet|
     client.favorite(tweet.id)
     time = DateTime.now
     client.update("#{tweet.user.name}さんが#{time.hour+9}時#{time.minute}分#{time.second}秒に呟きました。")
-    end
     if tweet.user.screen_name == "beauty_master_1"
       client.retweet(tweet.id)
       # client.update("@nukkoron \n 新しい投稿です\n #{tweet_url}")
-    end 
-
+    end
   end
 end
 
