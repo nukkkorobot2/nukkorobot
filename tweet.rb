61a3313acdb15cdec64e24e1d9a20f021d0334d8
nukkorobot2 <ymneko3104snow@gmail.com>
2018-07-18 23:24:04 +0900

    9th commit

--- tweet.rb
+++ tweet.rb
@@ -24,9 +24,6 @@ stream_client.user do |tweet|
     tweet_url = "https://twitter.com/#{tweet.user.id}/status/#{tweet.id}"
     client.favorite(tweet.id)
     time = DateTime.now
-    if time.second.length == 1
-    client.update("#{tweet.user}さんが#{time.hour+9}時#{time.minute}分0#{time.second}秒に呟きました。")
-    else
     client.update("#{tweet.user}さんが#{time.hour+9}時#{time.minute}分#{time.second}秒に呟きました。")
     end
     if tweet.user.screen_name == "beauty_master_1"
