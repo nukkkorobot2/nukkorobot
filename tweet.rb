3174a049d4d9c7580e4ced6ebed43846b578420a
nukkorobot2 <ymneko3104snow@gmail.com>
2018-07-18 23:10:14 +0900

    9th commit

--- tweet.rb
+++ tweet.rb
@@ -19,21 +19,17 @@ stream_client = Twitter::Streaming::Client.new do |config|
   config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
 end
 
-loop do
-
+stream_client.user do |tweet|
   if tweet.is_a?(Twitter::Tweet)
     tweet_url = "https://twitter.com/#{tweet.user.id}/status/#{tweet.id}"
     client.favorite(tweet.id)
+    time = DateTime.now
+    client.update("#{time.hour}時#{time.minute}分#{time.second}秒です")
     if tweet.user.screen_name == "beauty_master_1"
       client.retweet(tweet.id)
       # client.update("@nukkoron \n 新しい投稿です\n #{tweet_url}")
     end 
-  end
 
-      time = DateTime.now
-      if time.second == 10
-          client.update("#{time.hour}時#{time.minute}分#{time.second}秒です")
   end
-
 end
 
