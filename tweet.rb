b55bb786a7e7c294fdc1df039749f4e595eab275
nukkorobot2 <ymneko3104snow@gmail.com>
2018-07-21 23:17:28 +0900

    10th commit

--- tweet.rb
+++ tweet.rb
@@ -25,7 +25,11 @@ stream_client.user do |tweet|
     if tweet.user.screen_name != "nukkoro_bot"
     client.favorite(tweet.id)
     time = DateTime.now
-    client.update("#{tweet.user.name}さんが#{time.hour+9}時#{time.minute}分#{time.second-1}秒に呟きました。")
+    if time.huor >= 15
+        client.update("#{tweet.user.name}さんが#{time.hour-15}時#{time.minute}分#{time.second-1}秒に呟きました。")
+    else
+        client.update("#{tweet.user.name}さんが#{time.hour+9}時#{time.minute}分#{time.second-1}秒に呟きました。")
+    end
     if tweet.user.screen_name == "beauty_master_1"
       client.retweet(tweet.id)
       # client.update("@nukkoron \n 新しい投稿です\n #{tweet_url}")
